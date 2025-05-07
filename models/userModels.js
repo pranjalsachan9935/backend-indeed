e68afe5deb6af303772c17c3804509992d7c89b6
Pranjal Sachan <146807790+pranjalsachan9935@users.noreply.github.com>
2025-05-08 00:59:33 +0530

    Update userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -21,6 +21,6 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const userModel = mongoose.model("user", userSchema);
+const userModel = mongoose.model("User", userSchema);
 
 module.exports = userModel;
